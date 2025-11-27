5ad1fc99e7b762e8b943c9b9eaf152fe4b6e2d41
maxrt <max.r.tkachuk@gmail.com>
2025-11-27 04:31:29 +0200

    Add debug options for paging debugging

--- toolchain/qemu.cmake
+++ toolchain/qemu.cmake
@@ -2,13 +2,15 @@ include_guard(GLOBAL)
 
 check_cmd_dep("qemu-system-x86_64 --help" WARN "install it if you want qemu emulator targets to run")
 
+# -d cpu,int,mmu -no-reboot -no-shutdown
+
 function(__qemu_setup)
 	add_custom_target(qemu-run-${PROJECT_NAME}
-		qemu-system-x86_64 -M q35 -cdrom ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.iso -d int,cpu_reset,guest_errors -monitor stdio -D ${CMAKE_BINARY_DIR}/qemu.log
+		qemu-system-x86_64 -M q35 -cdrom ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.iso -d int,cpu_reset,guest_errors,mmu -no-reboot -no-shutdown -monitor stdio -D ${CMAKE_BINARY_DIR}/qemu.log
 	)
 
 	add_custom_target(qemu-run-gdb-${PROJECT_NAME}
-		qemu-system-x86_64 -M q35 -cdrom ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.iso -d int,cpu_reset,guest_errors -monitor stdio -D ${CMAKE_BINARY_DIR}/qemu.log -s -S
+		qemu-system-x86_64 -M q35 -cdrom ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.iso -d int,cpu_reset,guest_errors,mmu -no-reboot -no-shutdown -monitor stdio -D ${CMAKE_BINARY_DIR}/qemu.log -s -S
 	)
 endfunction()
 
